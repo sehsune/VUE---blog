db44bde0b6686559e4818bfe2fd91a5c66246534
Rafal <you@example.com>
2022-02-17 18:50:35 +0100

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">Delete a post</button>
+  </div>
+</template>
+
+<script>
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
+export default {
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title: "Welcome to the blog",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 1,
+      },
+      {
+        title: "Best tips",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 2,
+      },
+    ]);
+    const showPosts = ref(true);
+    return { posts, showPosts };
+  },
+};
+</script>
